efe3714deb9e1a016dffaa987ebd5ddbdf685f41
Galimede <mat.degand@outlook.fr>
2023-04-18 16:06:54 +0200

    feat: add small message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.end(`Answered to "${req.url}"`)
+  res.end(`Answered to "${req.url} with love."`)
 })
 
 server.listen(process.env.PORT)
