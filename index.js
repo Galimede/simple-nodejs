589964d3b90e733dcf1de52f67fce725e22264d8
Galimede <mat.degand@outlook.fr>
2023-04-18 15:59:15 +0200

    feat: my awesome feat

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  res.end(`Answered by "${process.env.CC_APP_NAME}" to "${req.url}"`)
+  res.end(`Answered to "${req.url}"`)
 })
 
 server.listen(process.env.PORT)
